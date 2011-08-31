9f403e369c69982c1c4e6c9de976fc931922871c
Christoph Lorenz <clorenz71@googlemail.com>
2011-08-31 14:28:26 +0200

    initial checkin

---
+++ src/de/uhrenbastler/watchcheck/WatchCheckActivity.java
@@ -0,0 +1,13 @@
+package de.uhrenbastler.watchcheck;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WatchCheckActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
