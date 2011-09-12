47a6f72ba7937044a290c8e2c9995188f2623466
Christoph Lorenz <clorenz71@googlemail.com>
2011-09-12 22:19:54 +0200

    First design of log entry screen

--- src/de/uhrenbastler/watchcheck/LogActivity.java
+++ src/de/uhrenbastler/watchcheck/LogActivity.java
@@ -23,16 +23,24 @@ import de.uhrenbastler.watchcheck.data.Watch.Watches;
 
 public class LogActivity extends Activity {
 	
-	List<String> watches = new ArrayList<String>();
-    
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
-        setContentView(R.layout.log);  
+        setContentView(R.layout.log); 
         
-        // Get all watches of from the content provider
-        getAllWatchesFromDatabase();
+        Spinner positionSpinner = (Spinner) findViewById(R.id.logSpinnerPosition); 
+        ArrayAdapter<?> positionAdapter = ArrayAdapter.createFromResource( this,
+        		R.array.positions,android.R.layout.simple_spinner_item); 
+        positionAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        positionSpinner.setAdapter(positionAdapter); 
+        
+        Spinner temperatureSpinner = (Spinner) findViewById(R.id.logSpinnerTemperature); 
+        ArrayAdapter<?> temperatureAdapter = ArrayAdapter.createFromResource( this,
+        		R.array.temperatures,android.R.layout.simple_spinner_item); 
+        temperatureAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        temperatureSpinner.setAdapter(temperatureAdapter); 
+        
         
         // Cancel button just closes the activity
         Button buttonCancel = (Button) findViewById(R.id.buttonLogCancel);
@@ -44,32 +52,6 @@ public class LogActivity extends Activity {
                 
             }
         });
-        
-        ArrayAdapter<CharSequence> spinnerWatchesAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, watches);
-        spinnerWatchesAdapter.add("New watch");
-        Spinner spinnerWatches = (Spinner) findViewById( R.id.spinnerWatches );
-        spinnerWatches.setAdapter( spinnerWatchesAdapter );
-        spinnerWatches.setOnItemSelectedListener(new OnItemSelectedListener() {
-        	 
-			@Override
-			// TODO: Hier wird auch "Selected watch new watch" selektiert!!"
-			public void onItemSelected(AdapterView<?> arg0, View arg1,
-					int position, long id) {
-				if ( position > watches.size())
-					Log.d("WatchCheck","Insert new watch");
-				else
-					Log.d("WatchCheck","Selected watch "+watches.get(position));
-				
-			}
-
-			@Override
-			public void onNothingSelected(AdapterView<?> arg0) {
-				// TODO Auto-generated method stub
-				
-			}
-        });
-
-        
         
         // OK button actually logs, displays an "OK" dialog, and after the dialog is acknowledged, closes
         // the activity
@@ -83,31 +65,4 @@ public class LogActivity extends Activity {
         });
         
     }
-
-    
-    // -> http://www.androidcompetencycenter.com/2009/01/basics-of-android-part-iv-android-content-providers/
-    private void getAllWatchesFromDatabase() {
-        Uri uriWatches = Watches.CONTENT_URI;
-        String[] columns = new String[] { Watches.WATCH_ID, Watches.NAME, Watches.SERIAL };
-        
-        Cursor cur = managedQuery(uriWatches, columns, null, null, Watches.NAME);
-        
-        if ( cur.moveToFirst()) {
-            Long id = null;
-            String name = null;
-            String serial = null;
-            do {
-                id = cur.getLong(cur.getColumnIndex(Watches.WATCH_ID));
-                name = cur.getString(cur.getColumnIndex(Watches.NAME));
-                serial = cur.getString(cur.getColumnIndex(Watches.SERIAL));
-                
-                Log.d("WatchCheck", "Found watch with id="+id+", name="+name+", serial="+serial);
-                
-                watches.add(name +(serial!=null?"("+serial+")":""));
-            } while ( cur.moveToNext());
-        }
-        
-        watches.add(getResources().getString(R.string.addWatch));
-    }
-
 }
